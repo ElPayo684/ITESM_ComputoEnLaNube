f5b89018d8c92adc1a1f59bd883a13524b98a032
Eduardo López <85975272+ElPayo684@users.noreply.github.com>
2025-01-26 17:32:11 -0600

    Actividad 1
    Actividad 1

---
+++ SolucionParalela_LopezGomez/SolucionParalela_LopezGomez.cpp
@@ -0,0 +1,51 @@
+// SolucionParalela_LopezGomez.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+#define N 1000
+#define chunk 100
+#define mostrar 10
+
+void imprimeArreglo(float *d);
+
+int main()
+{
+    cout << "|-------------------------------------------|" << endl;
+    cout << "|       Sumando Arreglos en Paralelo!       |" << endl;
+    cout << "|-------------------------------------------|\n" << endl;
+
+    float a[N], b[N], c[N];
+    int i;
+
+    for (i = 0; i < N; i++) {
+        a[i] = i * 10;
+        b[i] = (i + 3) * 3.7;
+    }
+    int pedazos = chunk;
+
+    #pragma omp parallel for \
+    shared(a, b, c, pedazos) private(i) \
+    schedule(static, pedazos)
+
+    for (i = 0; i < N; i++)
+        c[i] = a[i] + b[i];
+
+        cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << endl;
+        imprimeArreglo(a);
+        cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << endl;
+        imprimeArreglo(b);
+        cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << endl;
+        imprimeArreglo(c);
+}
+
+void imprimeArreglo(float *d) 
+{
+    for (int x = 0; x < mostrar; x++)
+        cout << d[x] << " - ";
+    cout << endl;
+}
+
